0739178110d114b58b8a181d32b5383f66a7519e
kelciemykell <kelciemykell@byui.edu>
2020-01-29 21:04:36 -0700

    week 4

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,4 +1,4 @@
-const hambutton = document.querySelector("navigation");
+const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
