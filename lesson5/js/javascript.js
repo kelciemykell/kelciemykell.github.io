be9d86beed5d1679eeff0bd9871caf0ef0eee20f
kelciemykell <kelciemykell@byui.edu>
2020-02-03 15:42:59 -0700

    week5

---
+++ lesson5/js/javascript.js
@@ -0,0 +1,7 @@
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+   /* document.getElementById("navigation").classList.toggle("hide");*/
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
