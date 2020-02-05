8b1639837c7915d9f69dd53934b06c3a5e6e3727
kelciemykell <kelciemykell@byui.edu>
2020-02-05 15:07:59 -0700

    week 5

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,7 +1,18 @@
 //const options={minute: "numeric", hour: "numeric", day: "numeric", month: "long", }
-const options= {year:"numeric"};
+/*const options= {year:"numeric"};
           
 document.getElementById('currentdate').textContent=new Date().toLocaleDateString("en-US", options);
 // const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
 //document.getElementById('lastModified').textContent=new Date(document.lastModified);
 document.getElementById('lastModified').textContent=document.lastModified;
+*/
+
+var d = new Date();
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var days = ['Sunday, ','Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, '];
+
+document.getElementById("currentDate").innerHTML = 
+days[d.getDay()] + ' '+ 
+d.getDate() +' '+ 
+months[d.getMonth()] +' '+ 
+d.getFullYear();
