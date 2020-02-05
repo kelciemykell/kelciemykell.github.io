3d4373f440d4a7d686bd5d81cbf82bb48101c240
kelciemykell <kelciemykell@byui.edu>
2020-02-05 15:21:31 -0700

    week 5

--- lesson5/js/javascript.js
+++ lesson5/js/javascript.js
@@ -5,12 +5,3 @@ function toggleMenu() {
    /* document.getElementById("navigation").classList.toggle("hide");*/
     document.querySelector(".navigation").classList.toggle("responsive");
 }
-var d = new Date();
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-var days = ['Sunday, ','Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, '];
-
-document.getElementById("todaysDate").innerHTML = 
-days[d.getDay()] + ' '+ 
-d.getDate() +' '+ 
-months[d.getMonth()] +' '+ 
-d.getFullYear();
---
+++ todaysDate.js
@@ -0,0 +1,9 @@
+var d = new Date();
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var days = ['Sunday, ','Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, '];
+
+document.getElementById("todaysDate").innerHTML = 
+days[d.getDay()] + ' '+ 
+d.getDate() +' '+ 
+months[d.getMonth()] +' '+ 
+d.getFullYear();
