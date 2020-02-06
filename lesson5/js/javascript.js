eac1a907b49794af3444efb672127c27a60f1b32
kelciemykell <kelciemykell@byui.edu>
2020-02-06 14:32:59 -0700

    week 5

--- lesson5/js/javascript.js
+++ lesson5/js/javascript.js
@@ -5,3 +5,9 @@ function toggleMenu() {
    /* document.getElementById("navigation").classList.toggle("hide");*/
     document.querySelector(".navigation").classList.toggle("responsive");
 }
+var day = new Date();
+var today = day.getDay();
+
+if(today == 4) {
+	document.getElementById("popup").className = "show";
+}
