5a5d115fbb232f034b5af6c06e56c85427b10706
kelciemykell <kelciemykell@byui.edu>
2020-02-13 12:35:55 -0700

    week 6

--- lesson6/js/winchill.js
+++ lesson6/js/winchill.js
@@ -1,7 +1,12 @@
 function () {
     document.getElementById('s').textContent = 45;
     document.getElementById('t').textContent = 15;
-    let factor = windChill(t, s);
+    if {
+        s<=50 && t>=3
+    let ws = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    }
+    else {
+        ws= "N/A"
     }
     
     function windChill(t, s) {
