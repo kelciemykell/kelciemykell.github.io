a8837b41793f68c5b996d28f510727fc576d147d
kelciemykell <kelciemykell@byui.edu>
2020-03-02 13:18:05 -0700

    week 9

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -4,20 +4,15 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {
-        const prophets = jsonObject['prophets'];
-  
-
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);}
-
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    for (let i = 0; i < prophets.length; i++) {
+      const prophets = jsonObject['prophets'];
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      card.appendChild(h2);
+      document.querySelector('div.cards').appendChild(card);
+    }
+    image.setAttribute('src', prophets[i].imageurl);
   });
-  
-image.setAttribute('src', prophets[i].imageurl);
+
