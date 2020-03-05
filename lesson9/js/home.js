5bcba92f98037e43765b2529aadb9ff34886887f
kelciemykell <kelciemykell@byui.edu>
2020-03-04 21:43:14 -0700

    week 9

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -1,49 +1,49 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
+    .then(function (response) {
+        return response.json();
+    })
 
-  .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
 
-    for (let i = 0; i < towns.length; i++) {
-        if(towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs"){
-      let h3 = document.createElement('h3'); //motto
-      let yearf = document.createElement('p'); 
-      let population = document.createElement('p')
-      let avgrain = document.createElement('p')
-      let image = document.createElement('img');
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2'); //name
-      
-      
-      h2.textContent = towns[i].name;
-      card.appendChild(h2);
-      document.querySelector('div.city').appendChild(card);
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
+                let h3 = document.createElement('h3'); //motto
+                let yearf = document.createElement('p');
+                let population = document.createElement('p');
+                let avgrain = document.createElement('p')
+                let image = document.createElement('img');
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2'); //name
 
-      h3.textContent = towns[i].motto;
-      card.appendChild(h3);
 
-      yearf.textContent = 'Year Founded: ' + towns[i].yearFounded;
-      yearf.setAttribute('class', 'yearf');
-      card.appendChild(yearf);
+                h2.textContent = towns[i].name;
+                card.appendChild(h2);
 
-      population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-      population.setAttribute('class', 'population');
-      card.appendChild(population);
+                h3.textContent = towns[i].motto;
+                card.appendChild(h3);
 
-      avgrain.textContent = 'Average Rainfall:' + ' ' + towns[i].averageRainfall;
-      avgrain.setAttribute('class', 'avgrain');
-      card.appendChild(avgrain);
+                yearf.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                yearf.setAttribute('class', 'yearf');
+                card.appendChild(yearf);
 
-      image.setAttribute('alt', towns[i].name);
-      card.appendChild(image);
-      photo.setAttribute('class', 'image');
+                population.textContent = 'Current Population: ' + towns[i].currentPopulation;
+                population.setAttribute('class', 'population');
+                card.appendChild(population);
 
-}
-}
+                avgrain.textContent = 'Average Rainfall:' + ' ' + towns[i].averageRainfall;
+                avgrain.setAttribute('class', 'avgrain');
+                card.appendChild(avgrain);
 
-});
+                image.setAttribute('alt', towns[i].name);
+                image.setAttribute('href', 'images/' + towns[i].photo);
+                image.setAttribute('class', 'image');
+                card.appendChild(image);
+                document.querySelector('div.city').appendChild(card);
+            }
+        }
+
+    });
