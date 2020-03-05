8fc75d5a8fc6907a994a0548cf265b2cd5ed1349
kelciemykell <kelciemykell@byui.edu>
2020-03-04 18:12:33 -0700

    week 9

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -4,6 +4,7 @@ fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
+
   .then(function (jsonObject) {
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject['towns'];
@@ -14,7 +15,7 @@ fetch(requestURL)
       let yearf = document.createElement('p'); 
       let population = document.createElement('p')
       let avgrain = document.createElement('p')
-      let photo = document.createElement('img');
+      let image = document.createElement('img');
       let card = document.createElement('section');
       let h2 = document.createElement('h2'); //name
       
@@ -26,11 +27,11 @@ fetch(requestURL)
       h3.textContent = towns[i].motto;
       card.appendChild(h3);
 
-      yearf.textContent = 'Year Founded:' + ' ' + towns[i].yearFounded;
+      yearf.textContent = 'Year Founded: ' + towns[i].yearFounded;
       yearf.setAttribute('class', 'yearf');
       card.appendChild(yearf);
 
-      population.textContent = 'Current Population:' + ' ' + towns[i].currentPopulation;
+      population.textContent = 'Current Population: ' + towns[i].currentPopulation;
       population.setAttribute('class', 'population');
       card.appendChild(population);
 
@@ -38,9 +39,9 @@ fetch(requestURL)
       avgrain.setAttribute('class', 'avgrain');
       card.appendChild(avgrain);
 
-      image.setAttribute('src', prophets[i].photo);
-      card.appendChild(photo);
-      photo.setAttribute('class', ' image' );
+      image.setAttribute('alt', towns[i].name);
+      card.appendChild(image);
+      photo.setAttribute('class', 'image');
 
 }
 }
