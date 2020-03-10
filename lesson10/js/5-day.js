d8fc59fd24c48e4c5b8a280b0918fdd81872722b
kelciemykell <kelciemykell@byui.edu>
2020-03-10 14:37:41 -0700

    week 10

--- lesson10/js/5-day.js
+++ lesson10/js/5-day.js
@@ -3,15 +3,14 @@ const apiURL = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=ff101
 const mydate = new Date();
 const y = mydate.getDay();
 console.log(y);
-const myweekday = new Array(7) {
-    myweekday[0] = "Sunday"
-    myweekday[1] = "Monday"
-    myweekday[2] = "Tuesday"
-    myweekday[3] = "Wednesday"
-    myweekday[4] = "Thursday"
-    myweekday[5] = "Friday"
-    myweekday[6] = "Saturday"
-};
+const myweekday = new Array(7); 
+    myweekday[0] = "Sunday";
+    myweekday[1] = "Monday";
+    myweekday[2] = "Tuesday";
+    myweekday[3] = "Wednesday";
+    myweekday[4] = "Thursday";
+    myweekday[5] = "Friday";
+    myweekday[6] = "Saturday";
 
 
 fetch(apiURL)
