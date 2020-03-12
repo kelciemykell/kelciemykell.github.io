f421f3493a6772a3cda05bb1bad7147bd78af080
kelciemykell <kelciemykell@byui.edu>
2020-03-12 15:13:29 -0700

    week 10

--- lesson10/js/5-day.js
+++ lesson10/js/5-day.js
@@ -17,8 +17,8 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);})
         
-        const fivedayforecast = jsObject.list.filter(x =>x.dt_txt.includes('18:00:00'));
-        console.log(jsObject);
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        console.log(fivedayforecast);
 
         for (let i=0; i<=fivedayforecast.length; i++){
             document.getElementById(`forecast${i+1}`).textContent = fivedayforecast[i].main.temp;};
