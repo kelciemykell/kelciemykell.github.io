31f93389e473501309b94538862e38f24c0f4ab8
kelciemykell <kelciemykell@byui.edu>
2020-03-12 22:11:43 -0700

    week 10

--- lesson10/js/practice.js
+++ lesson10/js/practice.js
@@ -18,49 +18,10 @@ fetch(apiURL1)
 
   const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ff101de3d4d514d1df9ef8df578576ab&units=imperial";
 
-  document.getElementById('label').apiURL.dt_txt;
-
   fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-	console.log(jsObject);
-	
-	const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-
-	for (let i = 0; i < jsObject.list.length; i++ ){
-		if (new Date(jsObject.list[i].dt_txt).getHours() == 18) {
-
-			var forecastAPI = jsObject.list[i];
-			
-			let div1 = document.createElement('div');
-			let div2 = document.createElement('div');
-			let head = document.createElement('bold');
-			let div3 = document.createElement('div');
-			//let image = document.createElement('img');
-			let temp = document.createElement('p');
-
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png'; // note the concatenation
-            const desc = jsObject.weather[0].description; // note how we reference the weather array
-            document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-            document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-            document.getElementById('icon').setAttribute('alt', desc);
-
-
-			head.textContent = dayOfWeek[new Date(apiURL.dt_txt).getDay()];
-			image.setAttribute('src', imgSource);
-			image.setAttribute('alt', apiURL.weather[0].description);
-			temp.innerHTML = apiURL.main.temp.toFixed(1) + " &#8457;";
-
-			div1.appendChild(div2);
-			div2.classList.add("bold");
-			div2.appendChild(head);
-			div3.classList.add("data");
-			div1.appendChild(div3);
-			div3.appendChild(image);
-			div3.appendChild(temp);
-
-			document.querySelector('div.forecastdiv').appendChild(div1);
-
-		}
-	}
+    console.log(jsObject);
+    
+document.getElementById('label').apiURL.dt_txt;
   });
