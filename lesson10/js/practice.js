56500d33a6921972920739feda58445ffca909ac
kelciemykell <kelciemykell@byui.edu>
2020-03-12 20:45:15 -0700

    week 10

--- lesson10/js/practice.js
+++ lesson10/js/practice.js
@@ -37,7 +37,7 @@ fetch(apiURL1)
 			let image = document.createElement('img');
 			let temp = document.createElement('p');
 
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[i].icon + '.png'; // note the concatenation
             const desc = jsObject.weather[0].description; // note how we reference the weather array
             document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
             document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
