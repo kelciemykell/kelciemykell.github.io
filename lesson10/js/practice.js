6d3ece7afcf9ab1f8bbac9c1e30ab496c2778846
kelciemykell <kelciemykell@byui.edu>
2020-03-12 21:30:39 -0700

    week 10

--- lesson10/js/practice.js
+++ lesson10/js/practice.js
@@ -37,13 +37,13 @@ fetch(apiURL1)
 			let image = document.createElement('img');
 			let temp = document.createElement('p');
 
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list.weather[0].icon + '.png'; // note the concatenation
+           /* const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list.weather[0].icon + '.png'; // note the concatenation
             const desc = jsObject.weather[0].description; // note how we reference the weather array
             document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
             document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
             document.getElementById('icon').setAttribute('alt', desc);
 
-			//const imageidentifier = forecastAPI.weather[0].main;
+		    const imageidentifier = forecastAPI.weather[0].main;
 			if (desc == "Clear"){
 				var imgsrc = ;
 			} 
@@ -61,13 +61,13 @@ fetch(apiURL1)
 			}
 			else {
 				var imgSource = "assets/mist.png"
-			}
+			}*/
 
 
-			head.textContent = dayOfWeek[new Date(forecastAPI.dt_txt).getDay()];
+			head.textContent = dayOfWeek[new Date(apiURL.dt_txt).getDay()];
 			image.setAttribute('src', imgSource);
-			image.setAttribute('alt', forecastAPI.weather[0].description);
-			temp.innerHTML = forecastAPI.main.temp.toFixed(1) + " &#8457;";
+			image.setAttribute('alt', apiURL.weather[0].description);
+			temp.innerHTML = apiURL.main.temp.toFixed(1) + " &#8457;";
 
 			div1.appendChild(div2);
 			div2.classList.add("Day");
