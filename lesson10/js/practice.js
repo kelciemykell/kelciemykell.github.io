62862fe37ff70afa4f8285a6b64530228d61903d
kelciemykell <kelciemykell@byui.edu>
2020-03-12 19:18:22 -0700

    week 10

--- lesson10/js/practice.js
+++ lesson10/js/practice.js
@@ -18,7 +18,7 @@ fetch(apiURL1)
 
   const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ff101de3d4d514d1df9ef8df578576ab&units=imperial";
 
-  fetch(apiForecastURL)
+  fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
 	console.log(jsObject);
