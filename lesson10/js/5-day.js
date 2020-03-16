b64a4dc9d3ad1d63b78491136a3c9a7962154a06
kelciemykell <kelciemykell@byui.edu>
2020-03-16 10:13:25 -0700

    week 10

--- lesson10/js/5-day.js
+++ lesson10/js/5-day.js
@@ -2,69 +2,67 @@
 const apiURL1 = "//api.openweathermap.org/data/2.5/weather?id=5604473&appid=ff101de3d4d514d1df9ef8df578576ab&units=imperial";
 
 fetch(apiURL1)
-  .then((response) => response.json())
-  .then((jsObject) => {
-	console.log(jsObject);
-	
-	const weatherAPI = jsObject;
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
 
-    document.getElementById('valCurrent').textContent = weatherAPI.weather[0].main;
-    document.getElementById('valHigh').textContent = weatherAPI.main.temp_max.toFixed(1);
-    document.getElementById('valHumid').textContent = weatherAPI.main.humidity;
-	document.getElementById('valWind').textContent = weatherAPI.wind.speed;
-	
-  });
+        const weatherAPI = jsObject;
+
+        document.getElementById('valCurrent').textContent = weatherAPI.weather[0].main;
+        document.getElementById('valHigh').textContent = weatherAPI.main.temp_max.toFixed(1);
+        document.getElementById('valHumid').textContent = weatherAPI.main.humidity;
+        document.getElementById('valWind').textContent = weatherAPI.wind.speed;
+
+    });
 
 
 
 
 const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ff101de3d4d514d1df9ef8df578576ab&units=imperial";
 
+const currentDay = new Date().getDay();
+const weekday = [];
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tue";
+weekday[3] = "Wed";
+weekday[4] = "Thu";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
+
+let count = currentDay;
+const forecast = jsObject.list;
+
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        
-        const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-        for (let i = 0; i < jsObject.list.length; i++ ){
-            if (new Date(jsObject.list[i].dt_txt).getHours() == 18) {
-    
-                var fiveDayForecast = jsObject.list[i];
-                
-                let div1 = document.createElement('div');
-                let div2 = document.createElement('div');
-                let head = document.createElement('strong');
-                let div3 = document.createElement('div');
+        for (let i = 0; i < jsObject.list; i++) {
+            if (forecast[i].dt_txt.includes("18:00:00")) {
+                if (count == 7) {
+                    count = 0;
+                }
+                let h4 = document.createElement('h4');
+                let div = document.createElement('div');
                 let image = document.createElement('img');
-                let temp = document.createElement('p');
+                let p = document.createElement('p');
 
+                h4.textContent = weekday[count];
+                div.appendChild(h4);
 
-                //weather icon
+                let icon = forecast[i].weather[0].icon;
+                image.setAttribute('src', 'https://openweathermap.org/img/w/' + icon + '.png');
+                image.setAttribute('alt', forecast[i].weather[0].description);
+                div.appendChild(image);
 
-                /*const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-                const desc = jsObject.weather[0].description; // note how we reference the weather array
-                document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-                document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-                document.getElementById('icon').setAttribute('alt', desc);*/
-        
-    
-    
-                head.textContent = dayOfWeek[new Date(fiveDayForecast.dt_txt).getDay()];
-                image.setAttribute('src', imgsrc);
-                image.setAttribute('alt', forecastAPI.weather[0].description);
-                temp.innerHTML = forecastAPI.main.temp.toFixed(1) + " &#8457;";
-    
-                div1.appendChild(div2);
-                div2.classList.add("Day");
-                div2.appendChild(head);
-                div3.classList.add("DayContent");
-                div1.appendChild(div3);
-                div3.appendChild(image);
-                div3.appendChild(temp);
-    
-                document.querySelector('div.forecastDiv').appendChild(div1);
-    
+                p.textContent = forecast[i].main.temp.toFixed(0)+' F'
+                div.appendChild(p);
+
+                document.querySelector('div.forecastDiv').appendChild(div);
+                count++;
+                    
+                }
             }
-        }
         });
+    
