e20e329dc6723568baabb450db5b32c7827d7a58
kelciemykell <kelciemykell@byui.edu>
2020-03-19 16:08:41 -0700

    week 11

--- lesson11/js/towneventsfish.js
+++ lesson11/js/towneventsfish.js
@@ -1,40 +1,4 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
- 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  
-  .then(function (jsonObject) {
-     
-    const towns = jsonObject['towns'];
-   for (let i = 0; i < towns.length; i++ ) {
-       if ( towns[i].name =="Fish Haven" ) {
-    let section = document.createElement('section');
-    let h4 = document.createElement('h4');
-    let x;
-    for (let x=0; x < towns[i].events.length; x++) {
-      h4.innerHTML += towns[i].events[x] + "<br>" ;
-      section.appendChild(h4);
-      
-
-
-    }
- 
-
- 
-
-document.querySelector('div.events').appendChild(section);
-}
-}
-});
-
-
-
-
-
-
-/*const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
     .then(function (response) {
