0139e520b2d1f2bef5ec93ab3afe186f6cc23401
kelciemykell <kelciemykell@byui.edu>
2020-03-19 16:05:37 -0700

    week 11

--- lesson11/js/towneventsfish.js
+++ lesson11/js/towneventsfish.js
@@ -1,4 +1,40 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+ 
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+     
+    const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       if ( towns[i].name =="Soda Springs" ) {
+    let section = document.createElement('section');
+    let h4 = document.createElement('h4');
+    let x;
+    for (let x=0; x < towns[i].events.length; x++) {
+      h4.innerHTML += towns[i].events[x] + "<br>" ;
+      section.appendChild(h4);
+      
+
+
+    }
+ 
+
+ 
+
+document.querySelector('div.events').appendChild(section);
+}
+}
+});
+
+
+
+
+
+
+/*const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
     .then(function (response) {
@@ -13,7 +49,7 @@ fetch(requestURL)
                 if (towns[i].name == "Fish Haven") {
                     let events = document.createElement('p');
 
-                    events.textContent = towns[i].events  + br;
+                    events.textContent = towns[i].events;
                     events.setAttribute('class', 'event');
                     let card = document.createElement('section');
                     card.appendChild(events);
