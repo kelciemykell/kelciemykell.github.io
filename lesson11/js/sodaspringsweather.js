615ff34ea77a856d8d1f5c22a58202969ee678c3
kelciemykell <kelciemykell@byui.edu>
2020-03-19 15:37:55 -0700

    week 11

---
+++ lesson11/js/sodaspringsweather.js
@@ -0,0 +1,15 @@
+const apiURL1 = "//api.openweathermap.org/data/2.5/weather?id=5607916&appid=ff101de3d4d514d1df9ef8df578576ab&units=imperial";
+
+fetch(apiURL1)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const weatherAPI = jsObject;
+
+        document.getElementById('valCurrent').textContent = weatherAPI.weather[0].main;
+        document.getElementById('valHigh').textContent = weatherAPI.main.temp_max.toFixed(1);
+        document.getElementById('valHumid').textContent = weatherAPI.main.humidity;
+        document.getElementById('valWind').textContent = weatherAPI.wind.speed;
+
+    });
