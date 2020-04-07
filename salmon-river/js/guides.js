a75d8a4ab9950f0b2f3e7fdca9483f47cc7548bc
kelciemykell <kelciemykell@byui.edu>
2020-04-07 10:42:14 -0700

    week 14

--- salmon-river/js/guides.js
+++ salmon-river/js/guides.js
@@ -1,29 +1,29 @@
-const apiURL= "https://kelciemykell.github.io/salmon-river/guides.json";
+const apiURL = "https://kelciemykell.github.io/salmon-river/guides.json";
 fetch(apiURL)
   .then(function (response) {
     return response.json();
   })
-  then(function (jsonObject) {
-    const guides = jsonObject['guides'];
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    
-    for (let i = 0; i < guides.length; i++) {
-      
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let info = document.createElement('info');
-      let image = document.createElement('img');
-      
-      h2.textContent = guides[i].name;
-      card.appendChild(h2);
-      document.querySelector('div.cards').appendChild(card);
+.then(function (jsonObject) {
+  const guides = jsonObject['guides'];
+  console.table(jsonObject); // temporary checking for valid response and data parsing
 
-      info.textContent = guides[i].info;
-      card.appendChild(info);
+  for (let i = 0; i < guides.length; i++) {
 
-      image.setAttribute('src', guides[i].imageurl);
-      card.appendChild(image);
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let info = document.createElement('info');
+    let image = document.createElement('img');
 
-    }
+    h2.textContent = guides[i].name;
+    card.appendChild(h2);
+    document.querySelector('div.cards').appendChild(card);
 
-  });
+    info.textContent = guides[i].info;
+    card.appendChild(info);
+
+    image.setAttribute('src', guides[i].imageurl);
+    card.appendChild(image);
+
+  }
+
+});
