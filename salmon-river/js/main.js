7727eabc1ea1c0a52a5014c400f76e02f9c03284
kelciemykell <kelciemykell@byui.edu>
2020-04-07 12:54:06 -0700

    week 14

--- salmon-river/js/main.js
+++ salmon-river/js/main.js
@@ -13,3 +13,14 @@ if(today == 5) {
 
 const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+var d = new Date();
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var days = ['Sunday, ','Monday, ','Tuesday, ','Wednesday, ','Thursday, ','Friday, ','Saturday, '];
+
+document.getElementById("todaysDate").innerHTML = 
+days[d.getDay()] + ' '+ 
+d.getDate() +' '+ 
+months[d.getMonth()] +' '+ 
+d.getFullYear();
